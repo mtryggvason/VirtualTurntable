7723ce5eb43f4316e1b5c465bbc10b1cd99c2d7f
mtryggvason <m.tryggvason@kiska.com>
2019-12-12 23:49:17 +0100

    add rotation instructions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import { throttle, debounce } from 'throttle-debounce';
 import { Player } from 'tone';
+import { ReactComponent as Rotate } from './svgs/rotate.svg';
+
 import Loader from 'react-loader-spinner'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
@@ -8,29 +10,25 @@ import './App.css';
 let player
 function App() {
   const [x, setX] = useState(0);
-  const [initialized, setInitialized] = useState(false);
+  const [player, setPlayer] = useState(false);
   const [playing, setPlaying] = useState(false)
   const [showMessage, setShowMessage] = useState(false);
   useEffect(() => {
-    if(player) return;
-    player = new Player({
-      onload: () => setInitialized(true),
-      'url': './Midday.[mp3|ogg]',
-      'loop': true,
-    }).toMaster();
-  })
-
-  useEffect(() => {
-  if (player) player.playbackRate = Math.abs(x / 45);
-  player.reverse = x > 0
+    if (player) {
+      const playbackRate = Math.abs(x / 45);
+      if (Math.abs(playbackRate - player.playbackRate) > 0.05) {
+        player.playbackRate = Math.abs(x / 45);
+        player.reverse = x > 0
+      }
+    } 
   }, [x]);
 
-
   const activateListener = () => {
-    player.start();
-    setPlaying(true);
+ 
     //player.autostart = true;
     if (DeviceMotionEvent.requestPermission) {
+      player.start();
+      setPlaying(true);
       DeviceMotionEvent.requestPermission()
       .then(response => {
         if (response == 'granted') {
@@ -42,6 +40,8 @@ function App() {
         }
       })
    } else if (window.DeviceOrientationEvent) {
+      player.start();
+      setPlaying(true);
       window.addEventListener('devicemotion', throttle(100, (e) => {
           const angularVelocity = e.rotationRate.alpa
           const rpm =  Math.round(e.rotationRate.gamma * 60 / 360);
@@ -52,26 +52,42 @@ function App() {
    }
   }
   return (
-    <div className="app">      
-      <img className={`vinyl-image center ${initialized ? '' :'loading'}`} src="/man.jpg" />
+    <div className="app">    
+      <PlayerComponent 
+        url="./Midday.[mp3|ogg]"  
+        onload={(player) => {
+          setPlayer(player)
+        }} 
+        loop/>  
+      <img className={`vinyl-image center ${player ? '' :'loading'}`} src="/man.jpg" />
       <div className="vinyl-dot center" />
       {x}
-    { !initialized && <Loader
+    { !player && <Loader
          type="Puff"
          className="center"
          color="#00BFFF"
          height={350}
          width={350}
       />}
-    { initialized && !playing  && <button className="center" onClick={activateListener}> Get Started</button>}
+    { player && !playing  && <button className="center" onClick={activateListener}> Get Started</button>}
+    {playing && <div className="center message">
+      <Rotate className="rotate rotating-svg"></Rotate>
+      Rotate the phone to hear the song play</div>}
+
     {showMessage && <div className="center message">Looks like your device does not support the <a href="https://caniuse.com/#feat=deviceorientation">Device Motion event</a>. <br/>Please try again with a mobile device</div>}
     </div>
   );
 }
 
-const PhoneSensor = (props) => {
-  
-
+const PlayerComponent = (props) => {
+  useEffect(() => {
+    player = new Player({
+      onload: () => props.onload(player),
+      'url': props.url,
+      'loop': props.loop,
+    }).toMaster();
+  },[]);
+  return null;
 }
 
 export default App;
