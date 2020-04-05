b900a682ad21e105a26274c8c7bf21fdb622409d
mtryggvason <mtryggvason@users.noreply.github.com>
2020-04-05 20:55:07 +0000

    deploy: d9de695ee8cea980d514691210b1eaf6e8a13429

---
+++ precache-manifest.72357463302239b06d36582cd74c9312.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e8f08639b4fae62ba3d12e07c498f9",
+    "url": "/VirtualTurntable/index.html"
+  },
+  {
+    "revision": "6b7329ea0f4e0087ba2d",
+    "url": "/VirtualTurntable/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "3dbd87278b164e912a79",
+    "url": "/VirtualTurntable/static/css/main.dad86d6c.chunk.css"
+  },
+  {
+    "revision": "6b7329ea0f4e0087ba2d",
+    "url": "/VirtualTurntable/static/js/2.bbe6a5ce.chunk.js"
+  },
+  {
+    "revision": "a407e491bd78a1b12dd3562fc67e366e",
+    "url": "/VirtualTurntable/static/js/2.bbe6a5ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dbd87278b164e912a79",
+    "url": "/VirtualTurntable/static/js/main.138cc173.chunk.js"
+  },
+  {
+    "revision": "be81e426bf8a2ae95b56",
+    "url": "/VirtualTurntable/static/js/runtime-main.e3292aa0.js"
+  },
+  {
+    "revision": "b424f259f0d71a06d6b2380b55f85e3f",
+    "url": "/VirtualTurntable/static/media/rotate.b424f259.svg"
+  }
+]);
