ef5f4fc23bed1b059fb0fa37ab091fbd6c4c2129
mtryggvason <m.tryggvason@kiska.com>
2020-04-26 23:56:03 +0200

    update reverse function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { fromEvent } from 'rxjs';
-import { throttleTime } from 'rxjs/operators';
+import React, { useState, useEffect } from "react";
+import { fromEvent } from "rxjs";
+import { throttleTime } from "rxjs/operators";
 
-import { throttle } from 'throttle-debounce';
-import { isMobile } from 'react-device-detect';
-import { Player, Buffer } from 'tone';
-import Loader from 'react-loader-spinner'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-import { ReactComponent as Rotate } from './svgs/rotate.svg';
-import './App.css';
-import  NoSleep from 'nosleep.js';
+import { isMobile } from "react-device-detect";
+import { Player, Buffer } from "tone";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { ReactComponent as Rotate } from "./svgs/rotate.svg";
+import "./App.css";
+import NoSleep from "nosleep.js";
 
 let reversed = false;
 let buffers = [];
@@ -21,8 +20,8 @@ const noSleep = new NoSleep();
  *  @private
  *  @return {Tone.Buffer} this
  */
-Buffer.prototype._reverse = function(){
-	if (this.loaded) {
+Buffer.prototype._reverse = function () {
+  if (this.loaded) {
     reversed = !reversed;
     if (reversed) {
       this._buffer.copyToChannel(reversedBuffers[0], 0, 0);
@@ -31,88 +30,124 @@ Buffer.prototype._reverse = function(){
       this._buffer.copyToChannel(buffers[0], 0, 0);
       this._buffer.copyToChannel(buffers[1], 1, 0);
     }
-	}
-	return this;
+  }
+  return this;
 };
 
-if (window.location.protocol != 'https:') {
-  window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+if (window.location.protocol !== "https:") {
+  /* window.location.href =
+    "https:" + window.location.href.substring(window.location.protocol.length);*/
 }
 
 const setRPM = (gamma, player) => {
-  const rpm =  Math.round(gamma * 60 / 360);
+  const rpm = Math.round((gamma * 60) / 360);
   if (player) {
     const playbackRate = Math.abs(rpm / 45);
     if (playbackRate > 0.05) {
       player.playbackRate = playbackRate;
-
     } else {
       player.playbackRate = 0;
     }
-    player.reverse = rpm > 0
-  } 
-}
-
-const initialState = {
-  playing: false,
-  player: null,
-  showMessage: false
+    player.reverse = rpm > 0;
+    if (player.reverse) {
+      const offset = player.buffer.toSeconds();
+      player.start(offset);
+    }
+  }
 };
 
 function App() {
   const [player, setPlayer] = useState(false);
-  const [playing, setPlaying] = useState(false)
+  const [playing, setPlaying] = useState(false);
   const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
     //setShowMessage(!window.DeviceOrientationEvent || !isMobile);
-  });
+  }, []);
 
   const activateListener = async () => {
     player.playbackRate = 1;
-    setPlaying(true)
-    if (window.DeviceOrientationEvent && isMobile) {
-      const response = DeviceMotionEvent.requestPermission ? await DeviceMotionEvent.requestPermission() : 'granted';
-      if (response === 'granted') {
+    setPlaying(true);
+    if (window.DeviceOrientationEvent) {
+      const response = DeviceMotionEvent.requestPermission
+        ? await DeviceMotionEvent.requestPermission()
+        : "granted";
+      if (response === "granted") {
         player.start();
         noSleep.enable();
         player.playbackRate = 0;
         setPlaying(true);
-        const stream = fromEvent(window, 'devicemotion').pipe(throttleTime(20));
-        stream.subscribe(e => setRPM(e.rotationRate.gamma, player));            
+        player.context.updateInterval = 0.01;
+        const stream = fromEvent(window, "devicemotion").pipe(throttleTime(5));
+        stream.subscribe((e) => setRPM(e.rotationRate.gamma, player));
       }
     }
   };
   const reverse = () => {
-    player.reverse = !player.reverse
-  }
-  const devTools = (<>
-   <button onClick={reverse}>REVERSE </button>
-      <input onChange={(event) => player.playbackRate = event.target.value} step="0.05" type="range" min="0" max="1" class="slider" id="myRange" />)
-     </>);
+    player.reverse = !player.reverse;
+  };
+
+  const devTools = (
+    <>
+      <button onClick={reverse}>REVERSE </button>
+      <input
+        onChange={(event) => (player.playbackRate = event.target.value)}
+        step="0.05"
+        type="range"
+        min="0"
+        max="1"
+        class="slider"
+        id="myRange"
+      />
+      )
+    </>
+  );
 
   return (
-    <div className="app">    
-      <PlayerComponent 
-        url="./Midday.[mp3|ogg]"  
+    <div className="app">
+      <PlayerComponent
+        url="./Midday.[mp3|ogg]"
         onload={(player) => {
-          setPlayer(player)
-        }} 
-        loop/>  
-      <img alt="vinyl" className={`vinyl-image center ${player ? '' :'loading'}`} src="man.jpg" />
+          setPlayer(player);
+        }}
+        loop
+      />
+      <img
+        alt="vinyl"
+        className={`vinyl-image center ${player ? "" : "loading"}`}
+        src="man.jpg"
+      />
       <div className="vinyl-dot center" />
-    { !player && <Loader
-         type="Puff"
-         className="center"
-         color="#00BFFF"
-         height={350}
-         width={350}
-      />}
-    { player && !playing  && <button className="center" onClick={activateListener}>Get Started</button>}
-    {playing && <div className="center message">
-      <Rotate className="rotate rotating-svg"></Rotate>
-      Turn of silent mode and rotate the phone to hear the song play</div>}
-    {showMessage && <div className="center message">Looks like your device does not support the <a href="https://caniuse.com/#feat=deviceorientation">Device Motion event</a>. <br/>Please try again with a mobile device</div>}
+      {!player && (
+        <Loader
+          type="Puff"
+          className="center"
+          color="#00BFFF"
+          height={350}
+          width={350}
+        />
+      )}
+      {player && !playing && (
+        <button className="center" onClick={activateListener}>
+          Get Started
+        </button>
+      )}
+      {playing && (
+        <div className="center message">
+          <Rotate className="rotate rotating-svg"></Rotate>
+          Turn of silent mode and rotate the phone to hear the song play
+        </div>
+      )}
+      {showMessage && (
+        <div className="center message">
+          Looks like your device does not support the{" "}
+          <a href="https://caniuse.com/#feat=deviceorientation">
+            Device Motion event
+          </a>
+          . <br />
+          Please try again with a mobile device
+        </div>
+      )}
     </div>
   );
 }
@@ -122,17 +157,17 @@ const PlayerComponent = (props) => {
     const player = new Player({
       onload: () => {
         for (var i = 0; i < player._buffer.numberOfChannels; i++) {
-          const buffer = player._buffer.getChannelData(i)
+          const buffer = player._buffer.getChannelData(i);
           buffers.push(buffer.slice());
-          reversedBuffers.push(buffer.slice().reverse())
+          reversedBuffers.push(buffer.slice().reverse());
         }
-        props.onload(player)
+        props.onload(player);
       },
-      'url': props.url,
-      'loop': props.loop,
+      url: props.url,
+      loop: props.loop,
     }).toMaster();
-  },[]);
+  }, []);
   return null;
-}
+};
 
 export default App;
